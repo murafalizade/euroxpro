6eb4f364a373e7ce3db6d70c575ae89d4fdb43a8
muradeliyev <muradaliyev2229@gmail.com>
2025-02-13 23:02:43 +0400

    cc images

--- src/pages/Home/ConferenceTeamScreen.tsx
+++ src/pages/Home/ConferenceTeamScreen.tsx
@@ -33,53 +33,53 @@ const ConferenceTeamScreen: FC = () => {
             <Row className="justify-content-center">
                 <h3 className={'text-center my-5'}>Core Team</h3>
                 <Col md={2} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Mohmmed Algelany'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Mohmmed Algelany'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={2} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Omar Ibrahim'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Omar Ibrahim'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={2} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Eleonara Pezzi'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Eleonara Pezzi'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={2} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Sertaç Gülbudak'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Sertaç Gülbudak'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={2} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Ahmed Ellithy'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Ahmed Ellithy'} lc={'Conference Manager'} position={'+33 1232 2323 21'}/>
                 </Col>
             </Row>
 
             <Row className="justify-content-center" style={{width: "80%"}}>
                 <h3 className={'text-center my-5'}>CC Team</h3>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Maria Timeea'} lc={'CCP'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Maria Timeea'} lc={'CCP'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Pietro'} lc={'CCM Events'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Pietro'} lc={'CCM Events'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Andrea'} lc={'CCVP Finance'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Andrea'} lc={'CCVP Finance'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Tudii'} lc={'CCM MKT'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Tudii'} lc={'CCM MKT'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Matilde'} lc={'CCM BD'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Matilde'} lc={'CCM BD'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Andrei'} lc={'CCM Logistics'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Andrei'} lc={'CCM Logistics'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Maciek'} lc={'CCM Events'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Maciek'} lc={'CCVP Events'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Ionela'} lc={'CCVP DxP'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Ionela'} lc={'CCVP DxP'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Feyza'} lc={'CCVP Logistics'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Feyza'} lc={'CCVP Logistics'} position={'+33 1232 2323 21'}/>
                 </Col>
                 <Col md={3} sm={6} className="d-flex justify-content-center mb-4">
-                    <DelegateCard img={''} name={'Francesco'} lc={'CCVP BD'} position={'+33 1232 2323 21'}/>
+                    <DelegateCard img={'/euroxpro.png'} name={'Francesco'} lc={'CCVP BD'} position={'+33 1232 2323 21'}/>
                 </Col>
             </Row>
         </motion.section>
