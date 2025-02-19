c0a521ac134ac11d5ae1f1dc1a801248505165a2
muradeliyev <muradaliyev2229@gmail.com>
2025-02-19 13:49:51 +0400

    navbar fixs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
-import {areas} from "../service/constants.ts";
+// import {areas} from "../service/constants.ts";
 import { Analytics } from "@vercel/analytics/react"
 
 
@@ -36,12 +36,12 @@ const CustomNavbar = () => {
                         <Nav.Link href="/merch" className="mx-3">Merch</Nav.Link>
                         <Nav.Link href="/" className="mx-3">Agenda</Nav.Link>
                         <Nav.Link href="/delegate-positions" className="mx-3 d-block d-md-none">Delegates</Nav.Link>
-                        <NavDropdown title="Delegates" id="delegates-dropdown" className="mx-3 d-none d-md-block">
-                            <NavDropdown.Item href={`/delegates`}>All</NavDropdown.Item>
-                            {areas.map(x=> (
-                                <NavDropdown.Item key={x}  href={`/delegates?position=${x}`}>{x}</NavDropdown.Item>
-                            ))}
-                        </NavDropdown>
+                        {/*<NavDropdown title="Delegates" id="delegates-dropdown" className="mx-3 d-none d-md-block">*/}
+                        {/*    <NavDropdown.Item href={`/delegates`}>All</NavDropdown.Item>*/}
+                        {/*    {areas.map(x=> (*/}
+                        {/*        <NavDropdown.Item key={x}  href={`/delegates?position=${x}`}>{x}</NavDropdown.Item>*/}
+                        {/*    ))}*/}
+                        {/*</NavDropdown>*/}
                         <Nav.Link href="/cc" className="mx-3">CC Team</Nav.Link>
                         <NavDropdown title="Help" id="help-dropdown" className="mx-3">
                             <NavDropdown.Item href="/resources">Resources</NavDropdown.Item>
