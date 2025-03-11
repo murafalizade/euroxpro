fbcb5abb61073fa55034c758998bf436d1205683
Murad <muradaliyev2229@gmail.com>
2025-03-11 19:19:32 +0400

    Update ResourcesScreen.tsx

--- src/pages/Home/ResourcesScreen.tsx
+++ src/pages/Home/ResourcesScreen.tsx
@@ -9,7 +9,7 @@ const resources = [
     { name: "TRANSPORTATION FORM", link: "https://aiesecitaly.typeform.com/to/xXIzPXfI" },
     { name: "TELEGRAM GROUP", link: "https://t.me/addlist/TMSQLPMYsKViMzQ8" },
     { name: "REGISTER NOW!", link: "https://aiesecitaly.typeform.com/REuroXpro25" },
-    { name: "AGENDA", link: "https://www.aes.ec/euroxpro-italy-2025-live-agenda" },
+    { name: "AGENDA", link: "https://www.aies.ec/euroxpro-italy-2025-live-agenda" },
     { name: "INVITATION LETTER FORM", link: "https://aiesecitaly.typeform.com/ILEuroXpro25" },
     { name: "BOOKLET", link: "https://drive.google.com/drive/u/1/folders/1a4XftlYlxY06dYLEQmFLk4OU1jSZolPJ?fbclid=PAZXh0bgNhZW0CMTEAAabk_mbG8Q9jEq7fOgMlU9m8EG7t6NK11UKPgxbboCqvg8uetr_BflERODc_aem_YnII5R1stvP-o1r0Q153wA" },
     { name: "PICTURES", link: "#" },
